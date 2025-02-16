34de7d32666fe2d4a915c03e88718dad85591a43
Guismx <guilhermesmx@gmail.com>
2025-02-16 13:11:00 -0300

    initializing projetc

---
+++ src/main/java/com/project/ybooks/YbooksApplication.java
@@ -0,0 +1,13 @@
+package com.project.ybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ybooks/YbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
