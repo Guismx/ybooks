db9cfb5a8e2de89006aa066fa21409bb77ec7619
Guismx <guilhermesmx@gmail.com>
2025-02-26 17:11:51 -0300

    add new attributes of user

--- src/main/java/com/project/ybooks/models/User.java
+++ src/main/java/com/project/ybooks/models/User.java
@@ -5,6 +5,9 @@ import com.project.ybooks.enums.UserEnum;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -18,11 +21,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String nome;
+    private Date data_nascimento;
+    private String cpf;
     private String username;
     private String password;
 
     @Enumerated(EnumType.STRING)
     UserEnum userEnum;
+
 
     public Long getId() {
         return id;
